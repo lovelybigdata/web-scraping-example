4f5de8bc4c176b22963688e32b9a402c7eb762c1
lovelybigdata <excel2power@live.com>
2014-05-14 12:16:04 +0800

    Update BeautifulSoup_example.py

--- BeautifulSoup_example.py
+++ BeautifulSoup_example.py
@@ -1,3 +1,18 @@
+'''這個範例是由  http://programminghistorian.org/lessons/intro-to-beautiful-soup 中所取得的
+   很簡明的說明了進行網路爬蟲的流程
+   簡單的來說：將網頁整個結構給捉下來→把不要的部分給刪掉→只保留下你想要的部份→輸出成csv檔案
+   其中'a'與'p'都是html的基本元素 'a'指的是超連結 'p"指的是段落
+   而'td' 'th' 'tr'都是屬於表格相關的元素
+   
+   另外因為網頁結構並沒有明確限制，所以常常有意外的狀況發生
+   像是本例中原始表格就有3個是沒有資料可以輸出的
+   所以也介紹了在python中要如何處理 
+   try:  
+   
+   except:
+   
+'''
+
 from bs4 import BeautifulSoup
 import csv
  
